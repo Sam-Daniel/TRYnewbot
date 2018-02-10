ed9cc4cd00a79b75839c22c1e7dfcb62bf9c5c12
convoluted <duminydaniel@gmail.com>
2018-02-11 03:43:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     <head>
         <title>Chat Bot User Interface</title>
          <link rel="stylesheet" type="text/css" href="style.css">
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	    <script type="text/javascript" src="js/ApiAi.min.js"></script>
-            <script type="text/javascript" src="js/app.js"></script>
+           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
+	    <script type="text/javascript" src="ApiAi.min.js"></script>
+            <script type="text/javascript" src="app.js"></script>
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
         <script type="text/javascript" src="credentials.js"></script>

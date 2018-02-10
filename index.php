0c99fe7fd00c70b450d27ffd5d5df5207eb589d2
convoluted <duminydaniel@gmail.com>
2018-02-11 03:19:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
          <link rel="stylesheet" type="text/css" href="style.css">
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <script type="text/javascript" src="js/ApiAi.min.js"></script>
+            <script type="text/javascript" src="js/app.js"></script>
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
         <script type="text/javascript" src="credentials.js"></script>

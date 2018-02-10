df777c2581ef10df66d969a6a8b2fac5c3265930
convoluted <duminydaniel@gmail.com>
2018-02-10 18:26:20 +0530

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,28 @@
+// Datanautix help bot
+//var accessToken = "b56ec2c85b2744ad81aeb6518d30a6ae";
+
+    //----------------------- Emoji Bot ----------------------- //
+    var credentialsAccessToken ="e6d0a7c6e0e741729839cadf0d76d987";
+
+    //var bot name is used for the firebase database
+    var credentialsBotName = "PB Chat";
+
+
+    // // HighSchoolAna(Dev)
+    // var credentialsAccessToken = "5489544adf6d490c8438cb7377f4bd60"
+
+    // var credentialsBotName = "High School Ana (Dev)"
+
+
+    var credentialsBaseUrl = "https://api.api.ai/v1/";
+
+    // Initialize Firebase
+   
+ var credentialsConfig = {
+         apiKey: "AIzaSyCIsXQiA05v3DCPiSPG39Jp3GUz-59MdF8",
+    authDomain: "pb-wswldm.firebaseapp.com",
+    databaseURL: "https://pb-wswldm.firebaseio.com",
+    projectId: "pb-wswldm",
+    storageBucket: "pb-wswldm.appspot.com",
+    messagingSenderId: "420438465811"
+    };
